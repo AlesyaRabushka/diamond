f48132816770bff59c9ffc7efd9dd266c82fecfb
Alesya <rabushka2014@mail.ru>
2023-10-04 00:54:36 +0300

    initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+
+const PORT = Number(process.env.PORT) || 3003;
+
+function bootstrap(){
+    try {
+        const app = express();
+
+        app.use(express.json());
+        app.use(express.urlencoded({
+            extended: true,
+        }))
+
+        app.listen(PORT, () => {
+            console.log(`[SERVER STARTED] server listen to http://localhost:${PORT}`)
+        })
+    } catch (error) {
+        console.log(error);
+
+        throw error;
+    }
+}
+
+bootstrap();
